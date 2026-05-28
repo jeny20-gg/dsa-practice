f9e6415609ec9502236af7dd83ccf7ff92ef5328
jeny20-gg <komjeny@gmail.com>
2026-05-28 23:13:49 +0530

    added right view of binary tree

---
+++ PROBLEM  Right view of binary tree.cpp
@@ -0,0 +1,45 @@
+PROBLEM : Right view of binary tree
+PLATFORM : GeeksforGeeks
+DIFFICULTLY : Easy
+
+
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> rightView(Node *root) {
+        //  code here
+        vector<int> ans;
+        if(root==NULL) return ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size=q.size();
+            for(int i=0;i<size;i++) {
+                Node* curr=q.front();
+                q.pop();
+                if(i==size - 1)
+                ans.push_back(curr->data);
+                if(curr->left)
+                q.push(curr->left);
+                if(curr->right)
+                q.push(curr->right);
+            }
+        }
+        return ans;
+    }
+};
