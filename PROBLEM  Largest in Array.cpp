bc2fc666e09ccc2c68bb63f77b1857990d66f403
jeny20-gg <komjeny@gmail.com>
2026-06-11 23:31:51 +0530

    added largest in array

---
+++ PROBLEM  Largest in Array.cpp
@@ -0,0 +1,20 @@
+// PROBLEM : Largest in Array 
+// PLATFORM : GeeksforGeeks
+// DIFFICULTLY : Easy
+
+
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int max=arr[0];
+        for(int i=0;i<n;i++) {
+            if(arr[i]>max) {
+            max=arr[i];
+            }
+        }
+        return max;
+    }
+};
