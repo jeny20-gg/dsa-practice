c371c8a18e4b2d567c6e1aab92609f6292237f57
jeny20-gg <komjeny@gmail.com>
2026-06-13 00:26:16 +0530

    added min and max in array

---
+++ PROBLEM  Min and Max in Array.cpp
@@ -0,0 +1,22 @@
+// PROBLEM : Min and Max in Array 
+// PLATFORM : GeeksforGeeks
+// DIFFICULTLY : Basic
+
+
+
+class Solution {
+  public:
+    vector<int> getMinMax(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int minval=arr[0],maxval=arr[0];
+        
+        for (int i=1;i<n;i++)
+        {
+            if (arr[i]<minval) minval=arr[i];
+            if (arr[i]>maxval) maxval=arr[i];        
+            
+        }
+        return {minval, maxval};
+    }
+};
