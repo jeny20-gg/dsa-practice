ff05a30c1f9c9863d5a5790964fc7cd7721aee06
jeny20-gg <komjeny@gmail.com>
2026-07-22 20:21:04 +0530

    added_minimize_the_heights_II

---
+++ PROBLEM  Minimize the Heights II.cpp
@@ -0,0 +1,22 @@
+// PROBLEM : Minimize the Heights II
+// PLATFORM : GeeksforGeeks
+// DIFFICULTLY : Medium
+
+
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int n=arr.size();
+        int ans=arr[n-1]-arr[0];
+        for(int i=1;i<n;i++) {
+            if(arr[i]-k<0) continue;
+            int mn=min(arr[0]+k, arr[i]-k);
+            int mx=max(arr[i-1]+k, arr[n-1]-k);
+            ans=min(ans,mx-mn);
+        }
+        return ans;
+    }
+};
