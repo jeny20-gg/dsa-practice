5567699d984e56caeb3ffc0b814c835b0e27ab07
jeny20-gg <komjeny@gmail.com>
2026-07-25 21:30:56 +0530

    added array subset

---
+++ PROBLEM  Array Subset.cpp
@@ -0,0 +1,26 @@
+// PROBLEM : Array Subset
+// PLATFORM : GeeksforGeeks
+// DIFFICULTLY : Basic
+
+
+
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        // Your code here
+        unordered_map<int,int> mp;
+        int n=a.size();
+        int m=b.size();
+        for(int i=0;i<n;i++) {
+            mp[a[i]]++;
+        }
+        for(int i=0;i<m;i++){
+            if(mp[b[i]]==0){
+                return false;
+            }
+            mp[b[i]]--;
+        }
+        return true;
+    }
+};
