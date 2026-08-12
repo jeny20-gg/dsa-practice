b946441282711846fd16f727d0a9376d74333677
jeny20-gg <komjeny@gmail.com>
2026-08-12 23:27:51 +0530

    added position of the set bit

---
+++ PROBLEM  Position of the Set Bit.cpp
@@ -0,0 +1,20 @@
+// PROBLEM : Position of the Set Bit
+// PLATFORM : GeeksforGeeks
+// DIFFICULTLY : Basic
+
+
+class Solution {
+  public:
+    int findPosition(int n) {
+        if (n == 0 || (n & (n - 1)) != 0)
+            return -1;
+        
+        int pos = 1;
+        while ((n & 1) == 0) {
+            n >>= 1;
+            pos++;
+        }
+        return pos;
+        
+    }
+};
