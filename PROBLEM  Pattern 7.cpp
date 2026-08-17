b56c4da381f5c6f4c82d82d2b2f7c9c6b3221c3c
jeny20-gg <komjeny@gmail.com>
2026-08-17 20:59:06 +0530

    added pattern 7

---
+++ PROBLEM  Pattern 7.cpp
@@ -0,0 +1,24 @@
+// PROBLEM : Pattern 7
+// PLATFORM : Striver (take you forward)
+// DIFFICULTLY : Easy
+
+
+
+
+class Solution {
+public:
+    void pattern7(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++) {
+                cout<<" ";
+            }
+            for(int j=0;j<2*i+1;j++) {
+                cout<<"*";
+            }
+            for(int j=0;j<n-i-1;j++) {
+                cout<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
