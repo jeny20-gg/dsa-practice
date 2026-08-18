3f4dd7e48ae9de8f8fb3f97b72d636c36cedecb9
jeny20-gg <komjeny@gmail.com>
2026-08-18 16:49:58 +0530

    added pattern 10

---
+++ PROBLEM  Pattern 10.cpp
@@ -0,0 +1,19 @@
+// PROBLEM : Pattern 10
+// PLATFORM : Striver (take you forward)
+// DIFFICULTLY : Easy
+
+
+
+class Solution {
+public:
+    void pattern10(int n) {
+        for(int i=1;i<=2*n-1;i++){
+            int start=i;
+            if(i>n) starts=2*n-1;
+            for(int j=1;j<=stars;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
