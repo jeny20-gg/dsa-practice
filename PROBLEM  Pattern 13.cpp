df260a4854f48c218382d7fe1e01d29354306191
jeny20-gg <komjeny@gmail.com>
2026-08-27 22:21:44 +0530

    added pattern 13

---
+++ PROBLEM  Pattern 13.cpp
@@ -0,0 +1,22 @@
+// PROBLEM : Pattern 13
+// PLATFORM : Striver (take you forward)
+// DIFFICULTLY : Easy
+
+
+
+
+
+
+class Solution {
+public:
+    void pattern13(int n) {
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<num;
+                num=num+1;
+            }
+            cout<<endl; 
+        }
+    }
+};
